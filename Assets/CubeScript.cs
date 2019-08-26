c766f664b10b2ca2bb09a8571cc3b95baad878de
Facundo D <genuinefafa@gmail.com>
2019-08-26 11:17:58 -0300

    Demo-2019-08-14

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    private Rigidbody _rb;
+    public KeyCode key1;
+    public KeyCode key2;
+    //public int hola;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("He corrido uwu");
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(key1))
+        {
+            //transform.
+            _rb.AddTorque(Vector3.right * 1000f * Time.deltaTime);
+        }
+        if (Input.GetKey(key2))
+        {
+            //transform.
+            _rb.AddTorque(-Vector3.right * 1000f * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/follow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour
+{
+    public GameObject myobject;
+    Vector3 _distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _distance = myobject.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 distance = Vector3.left * 5f;
+        Vector3 newPosition = myobject.transform.position - _distance;
+        transform.position = newPosition;
+    }
+}
